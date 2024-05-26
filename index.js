0f6f483e1d2d33c62cfe69f2d6e141adb21e5e2e
SerheyBr <sergeydrilkov@mail.ru>
2024-05-26 20:45:59 +0300

    add http

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const http = require("http");
 
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "content-type": "application/json",
+  });
+
+  if (req.url === "/users") {
+    return res.end(
+      JSON.stringify({
+        name: "Searhey",
+        age: 27,
+      })
+    );
+  }
+  if (req.url === "/posts") {
+    return res.end("POSTS");
+  }
+  res.end("fghjk");
+});
+
+server.listen(PORT, () => {
+  console.log(PORT);
+});
